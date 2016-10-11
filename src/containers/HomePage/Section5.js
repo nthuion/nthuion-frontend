b8c13d409f964186b2c89a02169ebcbad20efbd7
chengscott <chengscott@live.com>
2016-10-11 19:45:15 +0800

    update hackath0n time

--- src/containers/HomePage/Section5.js
+++ src/containers/HomePage/Section5.js
@@ -11,7 +11,7 @@ const Section5 = () => (
         <ScheduleRow from="08:50" to="09:20" event="報到" />
         <ScheduleRow from="09:20" to="09:55" event="開場" />
         <ScheduleRow from="09:55" to="10:00" event="選課預排系統經驗分享" />
-        <ScheduleRow from="10:00" to="10:45" event="3分鐘簡介提案" />
+        <ScheduleRow from="10:00" to="10:45" event="3 分鐘簡介提案" />
         <ScheduleRow from="10:45" to="11:00" event="三個關鍵字自我介紹" />
         <ScheduleRow from="11:00" to="12:00" event="分組媒合" />
         <ScheduleRow from="12:00" to="12:50" event="午餐、休息" />
@@ -24,7 +24,7 @@ const Section5 = () => (
     <img className={style.lineB} src={lineB} role="presentation" />
     <div className={style.scheduleTitle}>活動流程</div>
     <div className={style.location}>
-      <p>2016 / 9 / 28</p>
+      <p>2016 / 10 / 30</p>
       <p>國立清華大學資電館</p>
       <p>參加對象: 清大在校生</p>
       <p>報名費全免</p>
